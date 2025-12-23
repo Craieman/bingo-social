015c6b5901c74a37259cc6c83abd1ce31ed69909
Craieman <kiricrow@outlook.fr>
2025-12-23 22:59:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("createRoom").onclick = () => {
+  alert("Créer un salon (bientôt)");
+};
+
+document.getElementById("joinRoom").onclick = () => {
+  alert("Rejoindre un salon (bientôt)");
+};
